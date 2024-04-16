d1023cf7afe590db88099c92aa83769885ead3a4
zaners123 <zaners12345@gmail.com>
2024-04-15 17:23:34 -0700

    SockV6

--- examples/simplesock.js
+++ examples/simplesock.js
@@ -8,15 +8,15 @@ let k = new lib.KnitoutUtil();
 
 const COLOR_STRIPEA = '6';
 const X1=0;
-const X2=50; //V1 is 36
+const X2=40; //V1 is 36, V2 is 50
 
 k.run_autohook(k.wrap_serial(
 	k.gen_bindon_tube_open( k.STOCKINETTE,          COLOR_STRIPEA,                 X1, X2),
 	k.gen_arbtube(          k.STOCKINETTE,          COLOR_STRIPEA,   8, X1, X2),
 	k.gen_arbtube(          k.RIB1X1,               COLOR_STRIPEA,  64, X1, X2),
-	k.gen_arbtube(          k.FULLGAUGE_STOCKINETTE,COLOR_STRIPEA, 150, X1, X2),
+	k.gen_arbtube(          k.FULLGAUGE_STOCKINETTE,COLOR_STRIPEA, 250, X1, X2),
 	k.gen_heel(             k.FULLGAUGE_STOCKINETTE,COLOR_STRIPEA,                 X1, X2),
-	k.gen_arbtube(          k.FULLGAUGE_STOCKINETTE,COLOR_STRIPEA, 200, X1, X2),
+	k.gen_arbtube(          k.FULLGAUGE_STOCKINETTE,COLOR_STRIPEA,  90, X1, X2),
 	k.gen_bindoff_closed(COLOR_STRIPEA,                                            X1, X2,false)
 ),COLOR_STRIPEA)
 
